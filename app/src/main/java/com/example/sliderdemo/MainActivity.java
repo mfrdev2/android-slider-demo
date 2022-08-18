67754f980f379789781b1027471ccc695db4db32
FRabbi <mdfazlerabbi.diu@gmail.com>
2022-08-19 01:13:09 +0600

    initial commit

---
+++ app/src/main/java/com/example/sliderdemo/MainActivity.java
@@ -0,0 +1,144 @@
+package com.example.sliderdemo;
+
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.ViewPager;
+
+import jp.shts.android.storiesprogressview.StoriesProgressView;
+
+public class MainActivity extends AppCompatActivity implements StoriesProgressView.StoriesListener  {
+    private View view;
+    private static final int PROGRESS_COUNT = 6;
+
+    private StoriesProgressView storiesProgressView;
+    private ImageView image;
+
+    private int counter = 0;
+    private final int[] resources = new int[]{
+            R.drawable.sample1,
+            R.drawable.sample2,
+            R.drawable.sample3,
+            R.drawable.sample4,
+            R.drawable.sample5,
+            R.drawable.sample2,
+    };
+
+    private final long[] durations = new long[]{
+            500L, 1000L, 1500L, 4000L, 5000L, 1000,
+    };
+
+    long pressTime = 0L;
+    long limit = 500L;
+
+    private View.OnTouchListener onTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    pressTime = System.currentTimeMillis();
+                    storiesProgressView.pause();
+                    return false;
+                case MotionEvent.ACTION_UP:
+                    long now = System.currentTimeMillis();
+                    storiesProgressView.resume();
+                    return limit < now - pressTime;
+
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       /* getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN );*/
+        view = findViewById(R.id.image);
+        view.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LOW_PROFILE|
+                View.SYSTEM_UI_FLAG_FULLSCREEN|
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE|
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY|
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        );
+    //    blackIconStatusBar(this, R.color.colorAccent);
+
+        storiesProgressView = (StoriesProgressView) findViewById(R.id.stories);
+        storiesProgressView.setStoriesCount(PROGRESS_COUNT);
+        storiesProgressView.setStoryDuration(3000L);
+        // or
+        // storiesProgressView.setStoriesCountWithDurations(durations);
+        storiesProgressView.setStoriesListener(this);
+        storiesProgressView.startStories();
+
+        image = (ImageView) findViewById(R.id.image);
+        image.setImageResource(resources[counter]);
+
+        // bind reverse view
+        View reverse = findViewById(R.id.reverse);
+        reverse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                storiesProgressView.reverse();
+            }
+        });
+        reverse.setOnTouchListener(onTouchListener);
+
+        // bind skip view
+        View skip = findViewById(R.id.skip);
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                storiesProgressView.skip();
+            }
+        });
+        skip.setOnTouchListener(onTouchListener);
+    }
+
+    @Override
+    public void onNext() {
+        image.setImageResource(resources[++counter]);
+    }
+
+    @Override
+    public void onPrev() {
+        if ((counter - 1) < 0) return;
+        image.setImageResource(resources[--counter]);
+    }
+
+    @Override
+    public void onComplete() {
+        counter = 0;
+        image.setImageResource(resources[counter]);
+        storiesProgressView.startStories();
+    }
+
+    @Override
+    protected void onDestroy() {
+        // Very important !
+        storiesProgressView.destroy();
+        super.onDestroy();
+    }
+
+    @SuppressLint("InlinedApi")
+    public static void blackIconStatusBar(Activity activity, int color) {
+        activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        activity.getWindow().setStatusBarColor(ContextCompat.getColor(activity, color));
+    }
+}
